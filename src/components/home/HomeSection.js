a3892cb9aef04d74f848e91ad17141053ac5499a
kzaleskaa <kzaleska416@gmail.com>
2023-02-17 18:33:37 +0100

    create home screen

--- src/components/home/HomeSection.js
+++ src/components/home/HomeSection.js
@@ -1,26 +1,14 @@
 import { Link } from "react-scroll";
-import ReactTypingEffect from 'react-typing-effect';
-import LinksSection from "./LinksSection";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAnglesDown } from '@fortawesome/free-solid-svg-icons';
 import homeImage from "../../assets/images/home.png";
 
 import styles from "./HomeSection.module.css";
+import InformationBlock from "./InformationBlock";
 
 const HomeSection = (props) => {
     return <section id={props.id} className={styles.section}>
-        <div>
-            <h1>Hi There,</h1>
-            <h2>I'm Kasia Zaleska</h2>
-            <h1> Check my&nbsp;
-            <ReactTypingEffect
-                text={["projects", "courses", "education", "experience", "portfolio"]}
-                className={styles["typing-words"]}
-                eraseDelay={2000}
-                typingDelay={500}
-            /></h1>
-        <LinksSection className={styles.links} />
-        </div>
+        <InformationBlock />
         <img src={homeImage} alt="home profile" />
         <Link
             className={styles["scroll-link"]}
@@ -29,7 +17,7 @@ const HomeSection = (props) => {
             smooth
             offset={-70}
             duration={500}
-        >My portfolio <FontAwesomeIcon icon={faAnglesDown} className={styles.icon} /></Link>
+        >My portfolio &nbsp;<FontAwesomeIcon icon={faAnglesDown} className={styles.icon} /></Link>
     </section>
 }
 
