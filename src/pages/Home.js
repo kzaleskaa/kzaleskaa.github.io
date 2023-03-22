fd288690ba9c2372d01558d5ee32a575d30fff72
kzaleskaa <kzaleska416@gmail.com>
2023-03-22 23:18:52 +0100

    add animations

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAnglesDown } from "@fortawesome/free-solid-svg-icons";
 import homeImage from "../assets/images/home.png";
 import { Link, animateScroll as scroll, scroller } from "react-scroll";
+import Flip from "react-reveal/Flip";
+import Bounce from "react-reveal/Bounce";
 
 import InformationBlock from "../components/home/InformationBlock";
 import styles from "../components/home/HomeSection.module.css";
@@ -9,8 +11,12 @@ import styles from "../components/home/HomeSection.module.css";
 const Home = (props) => {
   return (
     <section id={props.id} className={styles.section}>
-      <InformationBlock />
-      <img src={homeImage} alt="home profile" />
+      <Bounce left>
+        <InformationBlock />
+      </Bounce>
+      <Flip right>
+        <img src={homeImage} alt="home profile" />
+      </Flip>
       <Link
         className={styles["scroll-link"]}
         to="projects"
